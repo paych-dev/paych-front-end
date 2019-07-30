e5eef52876199738647e1d2be61b6e5fc2fe372d
Xelanik <xelanik@gmail.com>
2019-07-30 21:38:27 +0300

    Add fonts, styling nav, layout and index components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Navigation.module.css'
+import logo from '../../assets/logo.png'
+import menuIcon from '../../assets/menu.png'
 
 const Navigation = () => {
   return (
-    <ul className='nav nav-pills'>
-      <li><Link to='/' activeClassName='active'>My Clubs</Link></li>
-      <li><Link to='/register' activeClassName='active'>Registration</Link></li>
-    </ul>
+    <nav className={styles.Nav}>
+      <div className={styles.Logo}>
+        <img src={logo} alt='logo' />
+      </div>
+      <ul className={styles.navWrap}>
+        <div className={styles.Logo}>
+          <img src={menuIcon} alt='logo' />
+        </div>
+        <li><Link to='/' activeClassName='active' className={styles.navElement}>My Clubs</Link></li>
+        <li><Link to='/register' activeClassName='active' className={styles.navElement}>Registration</Link></li>
+      </ul>
+    </nav>
   )
 }
 
