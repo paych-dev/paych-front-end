32197cd8c8c5875b3437c21052134e644e14b047
Alexander Shevchuk <xelanik@gmail.com>
2019-11-16 17:25:09 +0200

    New styles for feed element and layout

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,25 +1,50 @@
 import React from 'react'
+import {connect} from 'react-redux';
 //STYLES
 import styles from './Layout.module.scss'
 //COMPONENTS
 import Header from '../components/Layout/Header/Header';
 import Footer from '../components/Layout/Footer/Footer';
 import ModalStack from '../components/ModalStack/ModalStack';
+import SideMenu from "../components/Layout/SideMenu/SideMenu";
 
 class Layout extends React.Component {
-  render(){
-    return (
+  render() {
+    const {loggedIn} = this.props.auth;
+    const userLoggedIn = (
       <div className={styles.Wrap}>
-        <ModalStack />
-        <Header />
+        <ModalStack/>
+        <SideMenu/>
+        <main className={styles.Main}>
+          {this.props.children}
+        </main>
+      </div>
+    );
+
+    const userNotLoggedIn = (
+      <div className={styles.Wrap}>
+        <ModalStack/>
+        <Header/>
 
         <main className={styles.Main}>
           {this.props.children}
         </main>
-        <Footer />
+        <Footer/>
       </div>
-    )
-  };
+    );
+
+    if (loggedIn) {
+      return userLoggedIn
+    } else {
+      return userNotLoggedIn
+    }
+  }
+}
+
+const mapStateProps = state => {
+  return {
+    auth: state.auth
+  }
 };
 
-export default Layout
+export default connect(mapStateProps, null)(Layout)
--- src/pages/Feed/FeedElement/FeedElement.js
+++ src/pages/Feed/FeedElement/FeedElement.js
@@ -2,21 +2,32 @@ import React from 'react';
 import DateComponent from '../../../components/DateComponent/DateComponent';
 
 const FeedElement = ({content}) => {
+  const images = () => {
+    if (content.files[0]) {
+      return (
+        <div className='channel_image'>
+          {content.files[0] ? <img src={content.files[0].original_path} alt=''/> : null}
+        </div>
+      )
+    } else {
+      return null
+    }
+  };
   return (
     <div className='feed_card'>
       <div className='feed_header'>
-        <div className='feed_header_left'>
+        <div className='feed_header_wrap'>
           <div className='feed_logo'>
-            <img src={content.channel.avatar_image} alt='' />
+            <img src={content.channel.avatar_image} alt=''/>
           </div>
-          <h3 className='feed_title'>{content.channel.name}</h3>
+          <div className='feed-header-info'>
+            <h3 className='feed_title'>{content.channel.name}</h3>
+            <DateComponent date={content.created_at} page={'feed'}/>
+          </div>
         </div>
-        <DateComponent date = {content.created_at} page={'feed'}/>
       </div>
       <div className='feed_content'>
-        <div className='channel_image'>
-          { content.files[0] ? <img src={content.files[0].original_path} alt='' /> : null}
-        </div>
+          {images()}
         <div className='feed_text'>{content.text}</div>
       </div>
     </div>
